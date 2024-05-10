9567f16da2e5ff8a7b64e906b8684c6dfca8057a
sayemhossin <sayemhossin22@gmail.com>
2024-05-11 04:28:01 +0600

    added delete methode

--- src/Pages/MyQuery.jsx
+++ src/Pages/MyQuery.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 
 
-const MyQuery = ({query}) => {
+const MyQuery = ({ query }) => {
 
 
-    const handleDelete =(id) =>{
+    const handleDelete = (id) => {
         console.log(id)
         Swal.fire({
             title: "Are you sure?",
@@ -16,26 +16,26 @@ const MyQuery = ({query}) => {
             confirmButtonColor: "#3085d6",
             cancelButtonColor: "#d33",
             confirmButtonText: "Yes, delete it!"
-          }).then((result) => {
+        }).then((result) => {
             if (result.isConfirmed) {
-                fetch(`http://localhost:5000/allquery/${id}`,{
-                    method:'DELETE'
-                 })
-                 .then(res => res.json())
-                 .then(data =>{
-                    window.location.reload();
-                      if(data.deletedCount > 0){
-                        Swal.fire({
-                            title: "Deleted!",
-                            text: "Your query has been deleted.",
-                            icon: "success"
-                          });
-                         
-                      }
-                 })
+                fetch(`http://localhost:5000/allquery/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        window.location.reload();
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your query has been deleted.",
+                                icon: "success"
+                            });
+
+                        }
+                    })
             }
-          });
-    
+        });
+
     }
 
 
@@ -44,36 +44,36 @@ const MyQuery = ({query}) => {
 
     return (
         <div className=" lg:mx-40  py-4  border-2 rounded-xl">
-<div className="flex lg:gap-28 flex-col lg:flex-row">
-  <img src={query.photo} className="lg:max-w-sm mx-5 mt-3 rounded-lg " />
-  <div className="hero-content flex-col lg:flex-row">
-    <div>
-      <h1 className="text-3xl md:text-5xl font-bold">{query.product}</h1>
-      <h1 className="text-xl md:text-3xl font-bold">Brand: {query.brand}</h1>
-      <p className="py-6 text-xl font-semibold">Title: {query.title}</p>
-      <p>Added Date: {query.added_by.date}</p>
-  
-
-
-  <div className="flex items-center justify-around mt-8">
-    <Link to={`/querydetails/${query._id}`}> <button className="btn bg-blue-600 hover:bg-blue-800 text-xl text-blue-50">Details</button></Link>
-     <div className="space-x-7">
-     <Link to={`/queryupdate/${query._id}`}> <button className="tooltip text-xl " data-tip="Update"><FaUserEdit/></button></Link>
-     <button onClick={()=>handleDelete(query._id)} className="tooltip text-xl " data-tip="Delete"><FaTrashAlt /></button>
-     </div>
-    </div>
+            <div className="flex lg:gap-28 flex-col lg:flex-row">
+                <img src={query.photo} className="lg:max-w-sm mx-5 mt-3 rounded-lg " />
+                <div className="hero-content flex-col lg:flex-row">
+                    <div>
+                        <h1 className="text-3xl md:text-5xl font-bold">{query.product}</h1>
+                        <h1 className="text-xl md:text-3xl font-bold">Brand: {query.brand}</h1>
+                        <p className="py-6 text-xl font-semibold">Title: {query.title}</p>
+                        <p>Added Date: {query.added_by.date}</p>
 
 
 
+                        <div className="flex items-center justify-around mt-8">
+                            <Link to={`/querydetails/${query._id}`}> <button className="btn bg-blue-600 hover:bg-blue-800 text-xl text-blue-50">Details</button></Link>
+                            <div className="space-x-7">
+                                <Link to={`/queryupdate/${query._id}`}> <button className="tooltip text-xl " data-tip="Update"><FaUserEdit /></button></Link>
+                                <button onClick={() => handleDelete(query._id)} className="tooltip text-xl " data-tip="Delete"><FaTrashAlt /></button>
+                            </div>
+                        </div>
 
 
-    </div>
-  </div>
 
-             
-  </div>
-  
-</div>
+
+
+                    </div>
+                </div>
+
+
+            </div>
+
+        </div>
 
     );
 };
